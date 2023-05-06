e26649e6680f65a67999c5d953219fa097a465d8
potyoma <artyomnovosyolov254@gmail.com>
2023-05-07 00:46:19 +0400

    add shadows

--- src/script.js
+++ src/script.js
@@ -2,17 +2,21 @@ import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import * as dat from "lil-gui"
 
-const TEXTURES = ["ambientOcclusion", "color", "normal", "roughness"]
-const ADD_TEXTURES = ["alpha", "height", "metalness"]
+const TEXTURES = [
+  "ambientOcclusion",
+  "color",
+  "normal",
+  "roughness",
+  "alpha",
+  "height",
+  "metalness",
+]
 
-const loadTextures = (loader, objectName, loadAdditional) =>
-  (loadAdditional ? TEXTURES.concat(ADD_TEXTURES) : TEXTURES).reduce(
-    (acc, next) => {
-      acc[next] = loader.load(`/textures/${objectName}/${next}.jpg`)
-      return acc
-    },
-    {}
-  )
+const loadTextures = (loader, objectName) =>
+  TEXTURES.reduce((acc, next) => {
+    acc[next] = loader.load(`/textures/${objectName}/${next}.jpg`)
+    return acc
+  }, {})
 
 /**
  * Base
@@ -30,13 +34,25 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-const [bricksTextures, doorTextures, grassTextures] = [
-  "bricks",
-  "door",
-  "grass",
-].map(o => loadTextures(textureLoader, o, o === "door"))
+const [
+  bricksTextures,
+  doorTextures,
+  grassTextures,
+  graveTextures,
+  roofTextures,
+  bushTextures,
+] = ["bricks", "door", "grass", "grave", "roof", "bush"].map(o =>
+  loadTextures(textureLoader, o)
+)
 
 // Floor
+Object.keys(grassTextures).forEach(k => {
+  const texture = grassTextures[k]
+  texture.repeat.set(8, 8)
+  texture.wrapS = THREE.RepeatWrapping
+  texture.wrapT = THREE.RepeatWrapping
+})
+
 const floor = new THREE.Mesh(
   new THREE.CircleGeometry(10.5, 100),
   new THREE.MeshStandardMaterial({
@@ -45,6 +61,10 @@ const floor = new THREE.Mesh(
     roughnessMap: grassTextures.roughness,
     normalMap: grassTextures.normal,
   })
+)
+floor.geometry.setAttribute(
+  "uv2",
+  new THREE.Float32BufferAttribute(floor.geometry.attributes.uv.array, 2)
 )
 floor.rotation.x = -Math.PI * 0.5
 floor.position.y = 0
@@ -134,11 +154,30 @@ walls.geometry.setAttribute(
 walls.position.y = 2.6 / 2
 
 // Roof
+Object.keys(roofTextures).forEach(k => {
+  const texture = roofTextures[k]
+  texture.repeat.set(3, 3)
+  texture.wrapS = THREE.RepeatWrapping
+  texture.wrapT = THREE.RepeatWrapping
+})
+
 const roof = new THREE.Mesh(
-  new THREE.ConeGeometry(3.5, 2, 4),
-  new THREE.MeshStandardMaterial({ color: "#b35f45" })
+  new THREE.ConeGeometry(3.3, 1.3, 4, 20),
+  new THREE.MeshStandardMaterial({
+    color: "#b2b6b1",
+    map: roofTextures.color,
+    aoMap: roofTextures.ambientOcclusion,
+    normalMap: roofTextures.normal,
+    roughnessMap: roofTextures.roughness,
+    displacementMap: roofTextures.height,
+    displacementScale: 0.2,
+  })
 )
-roof.position.y = 3.6
+roof.geometry.setAttribute(
+  "uv2",
+  new THREE.Float32BufferAttribute(roof.geometry.attributes.uv.array, 2)
+)
+roof.position.y = 3.18
 roof.rotateY(Math.PI / 4)
 
 // Door
@@ -164,40 +203,64 @@ door.position.z = 2 + 0.01
 door.position.y = 1
 
 // Bushes
-const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
-const bushMaterial = new THREE.MeshStandardMaterial({
-  map: grassTextures.color,
-  aoMap: grassTextures.ambientOcclusion,
-  roughnessMap: grassTextures.roughness,
-  normalMap: grassTextures.normal,
+Object.keys(bushTextures).forEach(k => {
+  const texture = bushTextures[k]
+  texture.repeat.set(2, 2)
+  texture.wrapS = THREE.RepeatWrapping
+  texture.wrapT = THREE.RepeatWrapping
 })
 
-const _ = [
+const bushGeometry = new THREE.SphereGeometry(1, 32, 32)
+const bushMaterial = new THREE.MeshStandardMaterial({
+  map: bushTextures.color,
+  aoMap: bushTextures.ambientOcclusion,
+  roughnessMap: bushTextures.roughness,
+  normalMap: bushTextures.normal,
+  displacementMap: bushTextures.height,
+  displacementScale: 0.3,
+})
+bushGeometry.setAttribute(
+  "uv2",
+  new THREE.Float32BufferAttribute(bushGeometry.attributes.uv.array, 2)
+)
+
+const bushes = [
   [0.5, 0.8, 0.2, 2.2],
   [0.25, 1.4, 0.1, 2.1],
   [0.4, -0.8, 0.2, 2.2],
   [0.15, -1, 0.05, 2.6],
-].forEach(scalePosition => {
+].map(scalePosition => {
   const bush = new THREE.Mesh(bushGeometry, bushMaterial)
   bush.position.set(...scalePosition.slice(1))
   bush.scale.set(...Array.from({ length: 3 }, () => scalePosition[0]))
-  bush.geometry.setAttribute(
-    "uv2",
-    new THREE.Float32BufferAttribute(bushGeometry.attributes.uv.array, 2)
-  )
-  house.add(bush)
+  return bush
 })
 
-house.add(walls, roof, door, doorLight)
+house.add(walls, roof, door, doorLight, ...bushes)
 
 scene.add(house)
 
-// Graves
-const graves = new THREE.Group()
-const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
-const graveMaterial = new THREE.MeshStandardMaterial({ color: "#b2b6b1" })
+// Ghosts
+const ghosts = ["#ff00ff", "#00ffff", "#ffff00"].map(
+  color => new THREE.PointLight(color, 2, 3)
+)
 
-Array.from({ length: 60 }, () => {
+scene.add(...ghosts)
+
+// Graves
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial({
+  map: graveTextures.color,
+  aoMap: graveTextures.ambientOcclusion,
+  normalMap: graveTextures.normal,
+  roughnessMap: graveTextures.roughness,
+})
+graveGeometry.setAttribute(
+  "uv2",
+  new THREE.Float32BufferAttribute(graveGeometry.attributes.uv.array, 2)
+)
+
+const graves = Array.from({ length: 60 }, () => {
   const angle = Math.random() * Math.PI * 2
   const radius = 3 + Math.random() * 6.8
   const x = Math.sin(angle) * radius
@@ -207,10 +270,10 @@ Array.from({ length: 60 }, () => {
   grave.position.set(x, 0.3, z)
   grave.rotation.y = (Math.random() - 0.5) * 0.7
   grave.rotation.z = (Math.random() - 0.5) * 0.4
-  graves.add(grave)
+  return grave
 })
 
-scene.add(graves)
+scene.add(...graves)
 
 // Fog
 const fog = new THREE.Fog("#2f2837", 1, 15)
@@ -224,6 +287,18 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.setClearColor("#2f2837")
 
+// Do shadows
+renderer.shadowMap.enabled = true
+const shadows = [
+  moonLight,
+  doorLight,
+  ...ghosts,
+  ...graves,
+  walls,
+  ...bushes,
+].forEach(l => (l.castShadow = true))
+const receiveShadow = [floor, walls].map(o => (o.receiveShadow = true))
+
 /**
  * Animate
  */
@@ -231,6 +306,22 @@ const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+
+  // Update ghosts
+  ghosts.forEach((ghost, i) => {
+    const isOdd = i % 2 === 0
+    const angle = (isOdd ? elapsedTime : -elapsedTime) * (1.1 / (i + 1))
+    const x =
+      Math.cos(angle) *
+      (i === ghosts.length - 1 ? 7 + Math.sin(elapsedTime * 0.4) : 4 + i)
+    const z =
+      Math.sin(angle) *
+      (i === ghosts.length - 1 ? 7 + Math.sin(elapsedTime * 0.4) : 4 + i)
+    const y =
+      Math.sin(elapsedTime * (3 + i * 2)) +
+      (isOdd ? Math.sin(elapsedTime * 2.5) : 0)
+    ghost.position.set(x, y, z)
+  })
 
   // Update controls
   controls.update()
